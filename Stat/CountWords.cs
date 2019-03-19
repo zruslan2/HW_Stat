74228123ae0c09bce2c04cf750c1117396238f3a
Ruslan <Ruslan@RUSLAN-PC>
2019-03-19 20:49:30 +0600

    Add project files.

---
+++ Stat/CountWords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stat
+{
+    class CountWords
+    {
+        public static Dictionary<string, int> dict;
+        public CountWords()
+        {
+            dict = new Dictionary<string, int>();
+        }
+        public void AddWord(string word)
+        {
+            if (dict.ContainsKey(word))
+            {
+                dict[word] += 1;
+            }
+            else
+            {
+                dict.Add(word, 1);
+            }
+        }
+        public void Print()
+        {
+            foreach (var i in dict)
+            {
+                Console.WriteLine(" {0} - {1} раз.", i.Key, i.Value);
+            }
+        }
+    }
+}
---
+++ Stat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimiters = { ' ', ',', '.' };
+            string s = "Вот дом, Который построил Джек. А это пшеница, Кото­рая в темном  чулане хранится В доме, Который построил Джек. А это веселая птица­ синица, Которая часто вору­ет пшеницу, Которая в темном чулане хранится В доме, Который построил Джек";
+            string[] words = s.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            CountWords cw = new CountWords();
+            foreach (string i in words)
+            {
+                cw.AddWord(i);
+            }
+            cw.Print();
+        }
+    }
+}
